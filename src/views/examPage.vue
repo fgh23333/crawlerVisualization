f6a11e241b20e4491c1ca2bbdbde98ccf5bd2baa
fgh23333 <635262140@qq.com>
2023-12-21 10:11:53 +0800

    change

---
+++ src/views/examPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="examPage">
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="less">
+</style>
