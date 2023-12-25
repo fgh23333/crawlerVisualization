67d6ca604d5ad8a63b5cf39fe94a63218b0b15c0
fgh23333 <635262140@qq.com>
2023-12-25 21:42:11 +0800

    Update navMenu.vue

--- src/components/navMenu.vue
+++ src/components/navMenu.vue
@@ -1,8 +1,29 @@
 <template>
     <div id="navMenu">
         <el-menu class="el-menu-vertical-demo" :router="true" mode="horizontal">
-            <el-menu-item index="political">思政</el-menu-item>
-            <el-menu-item index="introduction">习概</el-menu-item>
+            <el-submenu index="political">
+                <template slot="title">思政</template>
+                <el-menu-item index="1">试卷1</el-menu-item>
+                <el-menu-item index="2">试卷2</el-menu-item>
+                <el-menu-item index="3">试卷3</el-menu-item>
+                <el-menu-item index="4">试卷4</el-menu-item>
+                <el-menu-item index="5">试卷5</el-menu-item>
+                <el-menu-item index="6">试卷6</el-menu-item>
+                <el-menu-item index="7">试卷7</el-menu-item>
+                <el-menu-item index="8">试卷8</el-menu-item>
+            </el-submenu>
+            <el-submenu index="introduction">
+                <template slot="title">习概</template>
+                <el-menu-item index="1">试卷1</el-menu-item>
+                <el-menu-item index="2">试卷2</el-menu-item>
+                <el-menu-item index="3">试卷3</el-menu-item>
+                <el-menu-item index="4">试卷4</el-menu-item>
+                <el-menu-item index="5">试卷5</el-menu-item>
+                <el-menu-item index="6">试卷6</el-menu-item>
+                <el-menu-item index="7">试卷7</el-menu-item>
+                <el-menu-item index="8">试卷8</el-menu-item>
+                <el-menu-item index="9">试卷9</el-menu-item>
+            </el-submenu>
         </el-menu>
     </div>
 </template>
