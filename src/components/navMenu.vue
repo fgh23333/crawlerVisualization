0016bdb1bec58a3a4072ce06b73309b7f703726e
fgh23333 <98164347+fgh23333@users.noreply.github.com>
2023-12-26 10:20:36 +0800

    change

--- src/components/navMenu.vue
+++ src/components/navMenu.vue
@@ -4,13 +4,13 @@
             <el-submenu index="political">
                 <template slot="title" class="title">思政</template>
                 <el-menu-item v-for="(item, i) in 8" :key="i">
-                    <router-link :to="'/examPage/' + 'political/' + i">试卷{{ i }}</router-link>
+                    <router-link :to="'/examPage/' + 'political/' + i" style="color: black;">试卷{{ i }}</router-link>
                 </el-menu-item>
             </el-submenu>
             <el-submenu index="introduction">
                 <template slot="title" class="title">习概</template>
-                <el-menu-item v-for="(item, i) in 8" :key="i">
-                    <router-link :to="'/examPage/' + 'introduction/' + index" v-for="(item, index) in 9" :key="index" style="color: black;">
+                <el-menu-item v-for="(item, i) in 9" :key="i">
+                    <router-link :to="'/examPage/' + 'introduction/' + index" :key="index" style="color: black;">
                         试卷{{ index }}
                     </router-link>
                 </el-menu-item>
