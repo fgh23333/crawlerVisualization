074c1c07cb43cae9a4bbd173232ab7cfa882f289
fgh23333 <635262140@qq.com>
2024-01-03 11:01:54 +0800

    Update navMenu.vue

--- src/components/navMenu.vue
+++ src/components/navMenu.vue
@@ -3,13 +3,13 @@
         <el-menu class="el-menu-vertical-demo" mode="horizontal" :router="true">
             <el-submenu index="political">
                 <template slot="title" class="title">思政试卷</template>
-                <el-menu-item v-for="(item, i) in 13" :key="i">
+                <el-menu-item v-for="(item, i) in 14" :key="i">
                     <router-link :to="'/examPage/' + 'political/' + i" style="color: black;">试卷{{ i }}</router-link>
                 </el-menu-item>
             </el-submenu>
             <el-submenu index="introduction">
                 <template slot="title" class="title">习概试卷</template>
-                <el-menu-item v-for="(item, index) in 25" :key="index">
+                <el-menu-item v-for="(item, index) in 26" :key="index">
                     <router-link :to="'/examPage/' + 'introduction/' + index" style="color: black;">
                         试卷{{ index }}
                     </router-link>
