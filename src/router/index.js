6de9c9d9da576842d98f2d368de8ba4cf24d5eb8
Yutian <nilei05@outlook.com>
2024-05-08 23:13:09 +0800

    首页

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import rightWrong from '@/views/rightWrong.vue'
 import singleChoice from '@/views/singleChoice.vue'
 import multipleChoice from '@/views/multipleChoice.vue'
 import newHome from "@/views/new/newHome.vue";
+import thehome from "@/views/new/thehome.vue";
 
 Vue.use(VueRouter)
 
@@ -47,7 +48,14 @@ const routes = [
   {
     path: '/test',
     name: 'newHome',
-    component: newHome
+    component: newHome,
+    children: [
+      {
+        path: '/test',
+        name: 'thehome',
+        component: thehome
+      }
+    ]
   }
 ]
 
