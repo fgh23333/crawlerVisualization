25ff20e95d14dfd8a8adc4c02ca86f169cdc7bbf
fgh23333 <635262140@qq.com>
2024-05-12 18:22:37 +0800

    back

--- src/views/new/newHome.vue
+++ src/views/new/newHome.vue
@@ -20,15 +20,25 @@
                                 <span>{{ list[i].subject }}</span>
                             </template>
                             <el-menu-item-group>
-                                <el-menu-item class="option" :index="'/newHome/subject/' + list[i].src">总题库</el-menu-item>
-                                <el-menu-item class="option" :index="'/newHome/rightWrong/' + list[i].src">判断题</el-menu-item>
-                                <el-menu-item class="option" :index="'/newHome/singleChoice/' + list[i].src">单选题</el-menu-item>
-                                <el-menu-item class="option" :index="'/newHome/multipleChoice/' + list[i].src">多选题</el-menu-item>
+                                <el-menu-item class="option"
+                                    :index="'/newHome/subject/' + list[i].src">总题库</el-menu-item>
+                                <el-menu-item class="option"
+                                    :index="'/newHome/rightWrong/' + list[i].src">判断题</el-menu-item>
+                                <el-menu-item class="option"
+                                    :index="'/newHome/singleChoice/' + list[i].src">单选题</el-menu-item>
+                                <el-menu-item class="option"
+                                    :index="'/newHome/multipleChoice/' + list[i].src">多选题</el-menu-item>
                             </el-menu-item-group>
                         </el-submenu>
                     </el-menu>
                 </div>
                 <div class="buttonContainer">
+                    <router-link to="/">
+                        <div class="tab" @click="open()">
+                            <img class="image" src="@/assets/icon/icon-favorites.svg">
+                            <div class="text">返回旧版</div>
+                        </div>
+                    </router-link>
                     <!-- <router-link to="/"> -->
                     <div class="tab" @click="open()">
                         <img class="image" src="@/assets/icon/icon-favorites.svg">
@@ -135,106 +145,6 @@ export default {
 </script>
 
 <style lang="less">
-.self-start {
-    align-self: flex-start;
-}
-
-.group_3 {
-    margin-right: 5px;
-    width: 99%;
-    height: 98vh;
-    display: flex;
-    justify-content: center;
-    /* 水平居中 */
-    align-items: center;
-    /* 垂直居中 */
-}
-
-.group_5 {
-    display: flex;
-    justify-content: left;
-    /* 水平居中 */
-}
-
-.group_6 {
-    display: flex;
-    justify-content: left;
-    /* 水平居中 */
-}
-
-.group_7 {
-    display: flex;
-    justify-content: left;
-    /* 水平居中 */
-}
-
-.flex-col {
-    display: flex;
-    flex-direction: column;
-}
-
-.text_6 {
-    color: #6c5dd3;
-    font-size: 72px;
-    font-family: HarmonyOSSansSC;
-    font-weight: 900;
-    line-height: 55px;
-    letter-spacing: 8px;
-}
-
-.group_4 {
-    margin-top: 64px;
-    width: 931px;
-}
-
-.text_8 {
-    color: #5f89d3;
-    font-size: 48px;
-    font-family: HarmonyOSSansSC;
-    font-weight: 900;
-    line-height: 46px;
-}
-
-.text_11 {
-    margin-top: 37px;
-    color: #828282;
-    font-size: 36px;
-    font-family: HarmonyOSSansSC;
-    font-weight: 700;
-    line-height: 43px;
-    text-align: left;
-}
-
-.group_5 {
-    opacity: 0;
-    animation: fadeInUp 0.5s ease-in-out forwards;
-    animation-delay: 0.5s;
-}
-
-.group_6 {
-    opacity: 0;
-    animation: fadeInUp 0.5s ease-in-out forwards;
-    animation-delay: 1s;
-}
-
-.group_7 {
-    opacity: 0;
-    animation: fadeInUp 0.5s ease-in-out forwards;
-    animation-delay: 1.5s;
-}
-
-@keyframes fadeInUp {
-    0% {
-        opacity: 0;
-        transform: translateY(50px);
-    }
-
-    100% {
-        opacity: 1;
-        transform: translateY(0);
-    }
-}
-
 .el-popover {
     border-radius: 24px !important;
     padding: 0 !important;
@@ -328,7 +238,7 @@ export default {
     }
 
     .nav {
-        height: 460px;
+        height: 410px;
         overflow: auto;
         text-align: center;
         -ms-overflow-style: none;
