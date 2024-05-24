e014c2776e04fc2b2d76238616744e495ffd7811
fgh23333 <635262140@qq.com>
2024-05-24 10:36:01 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+
   },
   getters: {
   },
@@ -13,5 +15,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [createPersistedState()]
 })
